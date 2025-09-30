ea8510e0f1c4661ec068365e8a26167fc574ea7e
yuechaguo <yuechao.guo@amd.com>
2025-09-30 17:19:33 +0800

    update umr doorbell script

--- static_umr_mi308x/dump_doorbell_ring.sh
+++ static_umr_mi308x/dump_doorbell_ring.sh
@@ -13,14 +13,22 @@ fi
 for xcc in {0,1,2,3}; do
     filename="sys_$(date +"%Y-%m-%d_%H_%M")_umr_doorbell_gpu${gpu}_xcc${xcc}.txt"
     echo "Generating $filename"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_CPC_STATUS 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_CPC_BUSY_STAT 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_CPC_STALLED_STAT1 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_STAT 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.GRBM_STATUS 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.GRBM_STATUS2 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_MEC1_INSTR_PNTR 2>&1 >> "${filename}"
+    ./umr -i "${g}" -vmp "${xcc}" -O bits -r *.*.CP_MEC2_INSTR_PNTR 2>&1 >> "${filename}"
     for me in {1,2}; do
         for pipe in {0,1,2,3}; do
             for queue in {0,1,2,3}; do
-                echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue}" 
+                # echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue}" 
                 echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue}" >> "${filename}"
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_PQ_DOORBELL_CONTROL 2>&1 >> "${filename}"
-                #echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue} VMID" >> "${filename}"
-                #./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_VMID 2>&1 >> "${filename}"
+                echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue} VMID" >> "${filename}"
+                ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_VMID 2>&1 >> "${filename}"
                 echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue} READ/WRITE" >> "${filename}"
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_PQ_RPTR 2>&1 >> "${filename}"
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_PQ_WPTR_LO 2>&1 >> "${filename}"
@@ -30,6 +38,8 @@ for xcc in {0,1,2,3}; do
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_MQD_BASE_ADDR_HI 2>&1 >> "${filename}"
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_PQ_BASE 2>&1 >> "${filename}"
                 ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_PQ_BASE_HI 2>&1 >> "${filename}"
+                echo "g:${g} xcc:${xcc} me:${me} pipe:${pipe} queue:${queue} ACTIVE" >> "${filename}"
+                ./umr -i "${g}" -vmp "${xcc}" -sb "${me}" "${pipe}" "${queue}" -O bits -r *.*.CP_HQD_ACTIVE 2>&1 >> "${filename}"
             done
         done
     done
